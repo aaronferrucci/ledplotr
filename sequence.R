ffd3b086aa17ed122a07c29bf9917090419fa60b
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-06-24 08:29:03 -0700

    Tidying, plotting.

--- sequence.R
+++ sequence.R
@@ -11,44 +11,41 @@
 # let position in the space on each axis be the number of LEDs of
 # lit LEDs of the axis color, there's a trajectory loop in the space.
 
-# Step 1: sequence of minute values, from 1:00 to 12:59
-sequence <- 60:(12*60+59)
-
 one_minutes <- function(x) {
-  return(x %% 60 %% 10);
+  return(x %% 60 %% 10)
 }
 
 ten_minutes <-  function(x) {
-  return(floor(x %% 60 / 10));
+  return(floor(x %% 60 / 10))
 }
 
 one_hours <- function(x) {
-  return(floor(x / 60) %% 10);
+  return(floor(x / 60) %% 10)
 }
 
 ten_hours <- function(x) {
-  return(floor(x / 600));
+  return(floor(x / 600))
 }
 
-digitize <- function(x) {
-  return(c(ten_hours(x), one_hours(x), ten_minutes(x), one_minutes(x)));
+get_rgbs <- function(sequence) {
+  rgbs <- data.frame(
+    red = ten_hours(sequence) + one_minutes(sequence),
+    green = one_hours(sequence),
+    blue = ten_minutes(sequence),
+    time = 1:length(sequence)
+  )
+
+  return(rgbs)
 }
 
-to_rgb <- function(digits) {
-  # indices go         1        2            3           4
-  #            ten-hours one-hours ten-minutes one-minutes
-  #                    R         G           B           R
-  return(c(digits[1] + digits[4], digits[2], digits[3]));
-}
+# sequence of minute values, from 1:00 to 12:59
+sequence <- 60:(12*60+59)
 
-get_rgbs <- function() {
-  digits <- lapply(sequence, FUN=digitize);
-  rgbs <- lapply(digits, FUN=to_rgb);
+# transform to RGB-space
+rgbs <- get_rgbs(sequence)
 
-  return(rgbs);
-}
-
-# To do: use a data structure appropriate to plot_ly(). data frame
-# with red, green, blue? Example here: https://plot.ly/r/3d-line-plots/
-#
+# Example here: https://plot.ly/r/3d-line-plots/
+library(plotly)
+p <- plot_ly(rgbs, x=red, y=green, z=blue, color=time, type="scatter3d")
+print(p)
 
